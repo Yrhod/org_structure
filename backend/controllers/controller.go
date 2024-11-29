d7b6de6e1a76eb7a99b79d584c7d0847c0823cfc
md-invt05 <92951375+md-invt05@users.noreply.github.com>
2024-11-29 21:00:58 +0300

    Initializing Go Server Using Gin

--- backend/controllers/controller.go
+++ backend/controllers/controller.go
@@ -1,1 +1,13 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetEmployees(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"employees": []string{"Alice", "Bob", "Charlie"},
+	})
+}
--- backend/routers/route.go
+++ backend/routers/route.go
@@ -1,1 +1,11 @@
 package routers
+
+import (
+	"org_structure_backend/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(router *gin.Engine) {
+	router.GET("/employees", controllers.GetEmployees)
+}
