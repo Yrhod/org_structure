878df006d7fef5c8fb296d5c98aa203da5b2f9b5
md-invt05 <92951375+md-invt05@users.noreply.github.com>
2024-11-30 18:56:44 +0300

    update search function

--- backend/models/employee.go
+++ backend/models/employee.go
@@ -10,6 +10,7 @@ type Employee struct {
 	ProjectID    int         `json:"project_id"`             // ID проекта, на котором работает сотрудник
 	ManagerID    *int        `json:"manager_id"`             // ID менеджера (если есть)
 	City         string      `json:"city"`                   // Город сотрудника
+	Phone        string      `json:"phone"`                  // Номер телефона сотрудника
 	Email        string      `json:"email"`                  // Электронная почта сотрудника
 	CalendarLink string      `json:"calendar_link"`          // Ссылка на календарь сотрудника
 	Manager      *Employee   `json:"manager,omitempty"`      // Менеджер сотрудника
